230c9f4a9022b35229f76d4e4d0c61b2cb5bebbf
kickbuttowski25 <vishalsinghbisht25@gmail.com>
2025-04-05 23:24:25 +0530

    Added Dynamic Programming code. Recursion with example

--- Dynamicprogramming/main.swift
+++ Dynamicprogramming/main.swift
@@ -6,6 +6,19 @@
 //
 
 import Foundation
+//recursion
+//Factorial
+// Recursion uses concept of call Stack to store all the calls or data/variables and remove each calls after they are called from stack
+func goHome(_ x: inout Int, _ home:inout Int) {
+    if x == home {
+        print("Reached Home")
+        return
+    }
+    x = x + 1
+    print(x)
+    goHome(&x, &home)
+}
 
-print("Hello, World!")
-
+var a = 1
+var b = 10
+goHome(&a, &b)
